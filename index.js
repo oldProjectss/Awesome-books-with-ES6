1671579b9727e6a7b3b8c39dcf0e8ef2b0d12cc4
Ismail courr <25227022+Ismailco@users.noreply.github.com>
2022-02-14 20:09:16 +0100

    Add luxon library

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { start } from './modules/start.js';
 import { listButton, addButton, contactButton } from './modules/DisplaySections.js';
 import { input } from './modules/input.js';
+import { DateTime } from './modules/luxon/build/es6/luxon.js';
 
 const addBtn = document.querySelector('.add_book_btn');
 const listBtn = document.querySelector('.list');
@@ -31,25 +32,9 @@ contactBtn.addEventListener('click', () => {
 const timeDisplay = document.querySelector('.time-display');
 
 const setTime = () => {
-  const date = new Date();
-  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-  const month = months[date.getMonth()];
-  const year = date.getFullYear();
-  const day = date.getDate();
-  let minutes = date.getMinutes();
-  const hours = date.getHours();
-  let seconds = date.getSeconds();
-
-  if (seconds.toString().length < 2) {
-    seconds = `0${seconds}`;
-  }
-  if (minutes.toString().length < 2) {
-    minutes = `0${minutes}`;
-  }
-
-  const time = `${month} ${day}th ${year}, ${hours}:${minutes}:${seconds}`;
+  const time = DateTime.now();
   timeDisplay.textContent = '';
-  timeDisplay.textContent = `${time}`;
+  timeDisplay.textContent = `${time.toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS)}`;
 };
 
 setInterval(setTime, 1000);
